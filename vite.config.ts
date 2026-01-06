e8a8d4f58bafc5645d4eb410f4804d51f28ea13d
georgeorlyissa-ctrl <georgeorlyissa@gmail.com>
2026-01-07 00:38:34 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [
     react({
-      exclude: /supabase\\/functions\\/server/,
+      exclude: /supabase\/functions\/server/,
     })
   ],
   
